0b5c9b52c0477b292bdb7fc58cd0772416a03288
aymen065 <caymen065@gmail.com>
2023-12-26 03:17:45 +0100

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  oauthUrl: 'http://20.37.3.15:5000/',
-  apiUrl: 'http://20.37.3.15:5000/',
+  oauthUrl: 'http://20.70.1.129:5000/',
+  apiUrl: 'http://20.70.1.129:5000/',
   production: true
   
 };
